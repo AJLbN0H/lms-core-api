04c22deb7aa123372df1ba809c5c9bbd6a9a58e5
Александр Вагин <sashavagin1987@gmail.com>
2026-03-12 00:39:30 +1000

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -85,13 +85,13 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("NAME"),
-        "USER": os.getenv("USER"),
-        "PASSWORD": os.getenv("PASSWORD"),
-        "HOST": os.getenv("HOST"),
-        "PORT": os.getenv("PORT"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('NAME', 'postgres'),
+        'USER': os.getenv('USER', 'postgres'),
+        'PASSWORD': os.getenv('PASSWORD', 'postgres'),
+        'HOST': os.getenv('HOST', 'db'),
+        'PORT': os.getenv('PORT', '5432'),
     }
 }
 
